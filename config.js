b0e4e31b3e233baf8e26d060885549acd375f549
Shake <143591158+Shake1227@users.noreply.github.com>
2025-09-08 17:20:39 +0900

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const EVENTS = [
         name: 'inメンシ「スーツ」', // 企画名
         logo: 'http://www.floral-village.info/up/1757264384.png', // 企画ロゴのURL (なければ空文字)
         skin: './skins/casino_suit.png', // 服装スキンのファイルパス
-        bgColor: '#2c3e50', // 背景色を指定する※背景画像がある場合は不要
+        // bgColor: '#2c3e50', // 背景色を指定する※背景画像がある場合は不要
         textColor: '#FFFFFF', // 背景画像を指定した際の、文字の色
-        // bgImage: 'https://example.com/path/to/your/image.jpg' // 背景画像を指定
+        bgImage: 'http://www.floral-village.info/up/1757319576.jpeg', // 背景画像を指定
     },
     // --- 新しい企画を追加する場合は、この下にコピーして追記 ---
     // {
