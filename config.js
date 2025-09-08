69ec34e4e21f848a9c793d301d493cdb72eda570
Shake <143591158+Shake1227@users.noreply.github.com>
2025-09-08 21:18:00 +0900

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,10 @@ const EVENTS = [
         name: 'inメンシ「スーツ」', // 企画名
         logo: 'http://www.floral-village.info/up/1757264384.png', // 企画ロゴのURL (なければ空文字)
         skin: './skins/casino_suit.png', // 服装スキンのファイルパス
-        // bgColor: '#2c3e50', // 背景色を指定する※背景画像がある場合は不要
-        textColor: '#FFFFFF', // 背景画像を指定した際の、文字の色
-        bgImage: 'http://www.floral-village.info/up/1757319576.jpeg', // 背景画像を指定
+        bgColor: '#2c3e50',
+        textColor: '#FFFFFF',
+        titleFont: 'Kiwi Maru', // タイトル用のフォント
+        descriptionFont: 'Yusei Magic', // 説明文用のフォント
     },
     // --- 新しい企画を追加する場合は、この下にコピーして追記 ---
     // {
@@ -17,9 +18,10 @@ const EVENTS = [
     //     name: '新しい企画',
     //     logo: '',
     //     skin: './skins/new_skin.png',
-    //     bgColor: '',
-    //     bgImage: '',
-    //     textColor: '' // 背景画像時の文字色
+    //     bgImage: 'https://example.com/image.jpg',
+    //     textColor: '#333',
+    //     titleFont: 'DotGothic16',
+    //     descriptionFont: 'Kaisei Opti',
     // }
     // ---------------------------------------------------------
 ];
