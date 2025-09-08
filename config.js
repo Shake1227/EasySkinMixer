cd5a1a827acba227767c4793f8e0f0a5542076bd
Shake <143591158+Shake1227@users.noreply.github.com>
2025-09-09 08:24:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,18 @@ const EVENTS = [
         lock: true, // trueにするとこの企画にロックがかかる
         lockSecret: 'UNLOCK-CASINO-SUIT', //企画ごとに異なる、秘密の言葉を設定
     },
+    {
+        id: 'example', // 企画のID (URLに使われます。半角英数字で)
+        name: 'サンプルページ（学校の制服）', // 企画名
+        logo: 'http://www.floral-village.info/up/1757264384.png', // 企画ロゴのURL (なければ空文字)
+        skin: './skins/casino_suit.png', // 服装スキンのファイルパス
+        bgImage: 'http://www.floral-village.info/up/1757334996.png',
+        textColor: '#000000', // タイトルのカラー
+        // titleFont: 'Kaisei Tokumin', // タイトル用のフォント
+        // descriptionFont: 'Kaisei HarunoUmi', // 説明文用のフォント
+        lock: false, // trueにするとこの企画にロックがかかる
+        lockSecret: 'UNLOCK-CASINO-SUIT', //企画ごとに異なる、秘密の言葉を設定
+    },
     // --- 新しい企画を追加する場合は、この下にコピーして追記 ---
     // {
     //     id: 'new_event',
